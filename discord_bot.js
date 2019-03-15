375748596dcedad1eaf08258fc17dcba71af1474
Jeong Hyeon <rhea31@naver.com>
2019-03-15 09:23:32 +0900

    음악 재생 추가, 좀 더 깔끔하게 다듬기

--- discord_bot.js
+++ discord_bot.js
@@ -4,8 +4,6 @@ class discord_bot {
         const { Client, RichEmbed } = require('discord.js');
         const EventEmitter = require('events');
         this.client = new Client();
-        this.room = "";
-        this.server = "";
         this.Emitter = new EventEmitter();
         this.connected = false;
 
@@ -18,7 +16,7 @@ class discord_bot {
         // Recv Message
         this.client.on("message", message => {
             if (!message.author.bot) {
-                this.Emitter.emit('message', message.author.username, message.content);
+                this.Emitter.emit('message', message.author.username, message);
             }
         });
     }
@@ -27,21 +25,36 @@ class discord_bot {
         this.client.login(token);
     }
 
-    set(server, room) {
-        this.server = server;
-        this.room = room;
-    }
-
-    send(message) {
+    send(server, room, message) {
         if (this.connected == true) {
-            const channel = this.client.channels.find(ch => ch.name === this.room);
+            const channel = this.client.channels.find(ch => ch.name === room);
             if (!channel) return;
-            if (channel.guild.name != this.server) return;
+            if (channel.guild.name != server) return;
             channel.send(message);
             return true;
         } else {
             return false;
         }
     }
+
+    translate(A, B, message, functions) {
+        var client_id = 'ObhHWl0y741h_9ky4Dbv';
+        var client_secret = 'RWoXmzjxa4';
+        var request = require('request');
+
+        var options = {
+            url: 'https://openapi.naver.com/v1/papago/n2mt',
+            form: { 'source': A, 'target': B, 'text': message.content },
+            headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
+        };
+
+        request.post(options, function (error, response) {
+            if (!error && response.statusCode == 200) {
+                var objBody = JSON.parse(response.body);
+                functions(objBody.message.result.translatedText, message);
+            }
+        });
+    }
+    
 }
 module.exports = discord_bot
--- main.js
+++ main.js
@@ -1,22 +1,91 @@
 'use strict';
 const discord_bot = require("./discord_bot.js");
 const { Client, RichEmbed } = require('discord.js');
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1 };
+const async = require('async');
 const bot = new discord_bot();
+const music = require("node-hashtable");
+const translate_mode = require("node-hashtable");
+var translate_A = "";
+var translate_B = "";
 bot.start("NTUyNjk4ODQ1MzY5NDY2ODkx.D2DUeg.-eaFse4wcJQo4HkWs-EEER1Px20"); // 서버의 Token
-bot.set("Bot_testing", "general"); // 이용할 서버와, 텍스트 채널
 
 // 메세지를 받음
 bot.Emitter.on('message', (user, message) => {
-    console.log(user + " : " + message);
-    bot.send(user + " : " + message);
+    var Array = message.content.split(' ');
+    switch (Array[0]) {
+        case "--재생":
+            if (message.guild) {
+                if (message.member.voiceChannel) {
+                    message.member.voiceChannel.join()
+                        .then(connection => {
+                            if (music.get(message.channel.guild.name) != undefined) {
+                                music.get(message.channel.guild.name).end();
+                            }
+                            message.channel.send("재생 시작");
+                            const stream = ytdl(Array[1], { filter: 'audioonly' });
+                            music.set(message.channel.guild.name, connection.playStream(stream, streamOptions));
+                        })
+                        .catch(console.log);
+                } else {
+                    message.channel.send("실패");
+                }
+            }
+            break;
+
+        case "--중지":
+            if (music.get(message.channel.guild.name) != undefined) {
+                music.get(message.channel.guild.name).end();
+            }
+            message.member.voiceChannel.leave();
+            break;
+
+        case "--번역모드":
+            if (translate_mode.get(message.channel.guild.name) == undefined)
+                translate_mode.set(message.channel.guild.name, -1);
+            translate_mode.set(message.channel.guild.name, translate_mode.get(message.channel.guild.name) * -1);
+
+            if (translate_mode.get(message.channel.guild.name) == -1)
+                message.channel.send("**번역모드를 껐습니다**");
+            if (translate_mode.get(message.channel.guild.name) == 1) {
+                message.channel.send("**번역모드를 켰습니다**");
+                if (Array.length == 3) {
+                    translate_A = Array[1];
+                    translate_B = Array[2];
+                }
+            }
+            break;
+
+        case "--상태":
+            var text = "**현재 Ohkay 의 상태는?**" + "\n";
+            if ((translate_mode.get(message.channel.guild.name) == 1)&& (translate_mode.get(message.channel.guild.name) != undefined))
+                text += "- 현재 **번역모드**가 켜져있습니다." + "\n";
+
+            if (music.get(message.channel.guild.name) != undefined)
+                text += "- 현재 노래를 재생 중입니다" + "\n";
+
+            message.channel.send(text);
+            break;
+
+        default:
+            if ((translate_mode.get(message.channel.guild.name) != undefined)&&(translate_mode.get(message.channel.guild.name) == 1)) {
+                bot.translate(translate_A, translate_B, message, translate);
+            }
+            break;
+    }
 });
 
 // 서버 시작시
 bot.Emitter.on('start', () => {
-    console.log("서버 시작");
     const embed = new RichEmbed()
         .setTitle('A slick little embed')
         .setColor(0xFF0000)
         .setDescription('Hello, this is a slick embed!');
-    bot.send(embed);
+    console.log("서버 시작");
+    bot.send("외국문화동아리", "bottest", embed);
 });
+
+function translate(String, message) {
+    message.channel.send(String);
+}
